91d438543eabfb74e894f23fd0ec833fa7cf8daf
Cristian Groza <cristian.groza@mail.mcgill.ca>
2023-06-21 09:14:01 -0400

    Version that takes into account reverse strands.

--- tagtobed/src/main.rs
+++ tagtobed/src/main.rs
@@ -1,8 +1,6 @@
-use std::env;
+
 extern crate bam;
 use crate::bam::RecordReader;
-use crate::bam::record::tags::TagViewer;
-use crate::bam::record::tags::TagValue;
 
 fn main() {
     let mut record = bam::Record::new();
@@ -16,25 +14,53 @@ fn main() {
                 match record.tags().get(b"MM") {
                     Some(bam::record::tags::TagValue::String(mm_tag_u8,  bam::record::tags::StringType::String))  => {
                         let mut mm_tag = std::str::from_utf8(mm_tag_u8).unwrap();
-                        println!("{:?}", mm_tag);
+                        // println!("{:?}", mm_tag);
                         // nothing to do if there are no base modifications
                         if mm_tag.len() == 0 {
                             break;
                         }
                         mm_tag = &mm_tag[0..mm_tag.len() - 1];
 
-                        let seq = String::from_utf8(record.sequence().to_vec()).unwrap();
+                        let seq : String;
+                        // base modifications coordinates are on the original strand
+                        if record.flag().is_reverse_strand() {
+                            seq = String::from_utf8(record.sequence().rev_compl(std::ops::RangeFull).collect()).unwrap();
+                        } else {
+                            seq = String::from_utf8(record.sequence().to_vec()).unwrap();
+                        }
+
                         // parse mm tag and Cs in sequence
                         // assumes only one type of modification
                         let c_vec : Vec<usize> = seq.match_indices("C").map(|c| c.0).collect();
-                        let mm_indices : Vec<i32> = mm_tag.split(',').skip(1).
-                            map(|i| { i.parse::<i32>().unwrap() }).collect();
+                        let mm_indices : Vec<usize> = mm_tag.split(',').skip(1).
+                            map(|i| { i.parse::<usize>().unwrap() }).collect();
+                        let max_c_vec_index : usize = mm_indices.iter().map(|a| { if *a == 0 { 1 } else { *a } } ).sum();
 
-                        println!("{}", seq);
+                        // make sure the base modification coordinates do not go past the last potentially modified bases
+                        assert!(max_c_vec_index < c_vec.len());
+
+                        // calculate position of modified bases
+                        let mut mod_bases : Vec<usize> = Vec::new();
+                        let mut i = 0;
+                        for mc in &mm_indices {
+                            // the current base is modified
+                            if *mc == 0 {
+                                mod_bases.push(c_vec[i]);
+                            // skip mc bases, then take the modified base
+                            } else {
+                                i = i + mc;
+                                mod_bases.push(c_vec[i]);
+                            }
+                            // we consumed the modified base
+                            i = i + 1;
+                        }
+
                         println!("{:?}", c_vec);
                         println!("{:?}", mm_indices);
+                        println!("{:?}", mod_bases);
+                        println!("----------------");
                     }
-                    _ => { panic!("No MM:Z tag."); }
+                    _ => { panic!("No MM:Z tag!"); }
                 }
 
             },
